b71095b4511cab849767fde42a9b48e59db2cad0
Zunawe <gyroscope15@gmail.com>
2021-04-16 09:50:36 -0700

    Update pre-commit hook to run subpackage tests

--- packages/chess-utils/lib/copyMove.ts
+++ packages/chess-utils/lib/copyMove.ts
@@ -2,7 +2,7 @@ import { Move, Coordinates } from './index'
 
 export const copyMove = (move: Move): Move => {
   return {
-    from: [new Coordinates(move.from[0].file, move.from[0].rank), { ...move.from[1]}],
-    to: [new Coordinates(move.to[0].file, move.to[0].rank), { ...move.to[1]}]
+    from: [new Coordinates(move.from[0].file, move.from[0].rank), { ...move.from[1] }],
+    to: [new Coordinates(move.to[0].file, move.to[0].rank), { ...move.to[1] }]
   }
 }
