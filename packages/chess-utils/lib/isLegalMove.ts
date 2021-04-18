486ecbbefe11e9bd2fc77207e238933b044045c7
Zunawe <gyroscope15@gmail.com>
2021-04-18 08:28:56 -0700

    Fix king able to castle while in check

--- packages/chess-utils/lib/isLegalMove.ts
+++ packages/chess-utils/lib/isLegalMove.ts
@@ -7,28 +7,32 @@ import {
   getLegalMoves,
   Move,
   Game,
-  Color
+  whoseTurn
 } from './index'
 
 export const isLegalMove = (move: Move, game: Game): boolean => {
-  const { moves } = game
-
-  const turn: Color = moves.length % 2 === 0 ? 'L' : 'D'
-  if (move.from[1].color !== turn) {
+  const color = move.from[1].color
+  if (color !== whoseTurn(game)) {
     return false
   }
 
   return getLegalMoves(move.from, game)
-    .filter((legalMove) => !isInCheck(move.from[1].color, applyMove(legalMove, game)))
+    // Move can't leave king in check
+    .filter((legalMove) => !isInCheck(color, applyMove(legalMove, game)))
     .filter((legalMove) => {
       if (isCastle(move)) {
+        // King can't castle while in check
+        if (isInCheck(color, game)) {
+          return false
+        }
+
+        // King can't cross a space that would put him in check during castle
         const direction = (legalMove.to[0].file - legalMove.from[0].file) / 2
         const testMove: Move = {
           from: legalMove.from,
           to: [new Coordinates(legalMove.from[0].file + direction, legalMove.to[0].rank), legalMove.to[1]]
         }
-
-        return !isInCheck(move.from[1].color, applyMove(testMove, game))
+        return !isInCheck(color, applyMove(testMove, game))
       }
 
       return true
