50e5756b0e630de3987218468e67deab691bd473
Zunawe <gyroscope15@gmail.com>
2021-04-18 08:16:13 -0700

    Set public path to /chess for reverse proxy

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,10 @@ const baseConfig = require('./webpack.common')
 const { merge } = require('webpack-merge')
 
 const config = {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    publicPath: '/chess'
+  }
 }
 
 module.exports = merge(baseConfig, config)
